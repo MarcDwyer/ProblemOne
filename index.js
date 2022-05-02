e5e62da59230f4c7222f19e8268993e36ddf9165
Marc Dwyer <marcdwyer1738@gmail.com>
2022-05-02 16:29:32 -0500

    added flattenBooking to mutateArray

--- index.js
+++ index.js
@@ -1,65 +1,81 @@
 var arr = [
   {
-    'guest_type': 'crew',
-    'first_name': 'Marco',
-    'last_name': 'Burns',
-    'guest_booking': {
-        'room_no': 'A0073',
-        'some_array': [7,2,4]
+    guest_type: "crew",
+    first_name: "Marco",
+    last_name: "Burns",
+    guest_booking: {
+      room_no: "A0073",
+      some_array: [7, 2, 4],
     },
   },
   {
-    'guest_type': 'guest',
-    'first_name': 'John',
-    'last_name': 'Doe',
-    'guest_booking': {
-        'room_no': 'C73',
-        'some_array': [1,3,5,2,4,3]
+    guest_type: "guest",
+    first_name: "John",
+    last_name: "Doe",
+    guest_booking: {
+      room_no: "C73",
+      some_array: [1, 3, 5, 2, 4, 3],
     },
   },
   {
-    'guest_type': 'guest',
-    'first_name': 'Jane',
-    'last_name': 'Doe',
-    'guest_booking': {
-        'room_no': 'C73',
-        'some_array': [1,3,5,2,4,3]
+    guest_type: "guest",
+    first_name: "Jane",
+    last_name: "Doe",
+    guest_booking: {
+      room_no: "C73",
+      some_array: [1, 3, 5, 2, 4, 3],
     },
   },
   {
-    'guest_type': 'guest',
-    'first_name': 'Albert',
-    'last_name': 'Einstein',
-    'guest_booking': {
-        'room_no': 'B15',
-        'some_array': [2,5,6,3]
+    guest_type: "guest",
+    first_name: "Albert",
+    last_name: "Einstein",
+    guest_booking: {
+      room_no: "B15",
+      some_array: [2, 5, 6, 3],
     },
   },
   {
-    'guest_type': 'crew',
-    'first_name': 'Jack',
-    'last_name': 'Daniels',
-    'guest_booking': {
-        'room_no': 'B15',
-        'some_array': [2,5,6,3]
+    guest_type: "crew",
+    first_name: "Jack",
+    last_name: "Daniels",
+    guest_booking: {
+      room_no: "B15",
+      some_array: [2, 5, 6, 3],
     },
   },
   {
-    'guest_type': 'guest',
-    'first_name': 'Alan',
-    'last_name': 'Turing',
-    'guest_booking': {
-        'room_no': 'B15',
-        'some_array': [2,5,6,3]
+    guest_type: "guest",
+    first_name: "Alan",
+    last_name: "Turing",
+    guest_booking: {
+      room_no: "B15",
+      some_array: [2, 5, 6, 3],
     },
   },
 ];
 
-function mutateArray(a) {
-    return a;
+function flattenBooking(booking) {
+  const guestBookingKey = "guest_booking";
+  if (guestBookingKey in booking) {
+    const someArray = booking[guestBookingKey]["some_array"];
+    delete booking[guestBookingKey];
+    booking["some_array"] = someArray;
+  }
+  return booking;
 }
 
-$(document).ready(function() {
-    $('#originalArray').html(JSON.stringify(arr, null, 2));
-    $('#resultsArray').html(JSON.stringify(mutateArray(arr), null, 2));
+function mutateArray(a) {
+  for (let index = 0; index < a.length; index++) {
+    const booking = a[index];
+
+    const newBooking = flattenBooking(booking);
+    a[index] = newBooking;
+  }
+  return a;
+}
+
+$(document).ready(function () {
+  $("#originalArray").html(JSON.stringify(arr, null, 2));
+  $("#resultsArray").html(JSON.stringify(mutateArray(arr), null, 2));
 });
